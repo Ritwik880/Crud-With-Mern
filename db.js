53f49977baae031fee7c875fa1de15309a196da7
Ritwik880 <rittiksinha12@gmail.com>
2021-07-23 08:38:06 +0530

    deleted

--- db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/postManagerDb',{useNewUrlParser:true, useUnifiedTopology:true},
-    err => {
-        if (!err)
-            console.log('Mongodb connection succeeded')
-        else {
-            console.log('Error while connection : ' + JSON.stringify(err, undefined, 2))
-        }
-    })
